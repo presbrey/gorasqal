0fd113ca2872058811c44885b3b7e78cb13c7cbb
joe <presbrey+git@gmail.com>
2015-02-15 20:34:14 -0500

    go1 compat + travis

--- gorasqal.go
+++ gorasqal.go
@@ -1,6 +1,6 @@
 package gorasqal
 
-// #cgo CFLAGS: -I/usr/local/include/rasqal -I/usr/local/include/raptor2
+// #cgo CFLAGS: -I/usr/local/include/rasqal -I/usr/local/include/raptor2 -I/usr/include/rasqal -I/usr/include/raptor2
 // #cgo LDFLAGS: -L/usr/local/lib -lrasqal -lraptor2
 // #include <stdlib.h>
 // #include <rasqal.h>
@@ -8,16 +8,18 @@ package gorasqal
 import "C"
 
 import (
-	"bitbucket.org/ww/goraptor"
+	"errors"
 	"log"
 	"os"
 	"sync"
 	"unsafe"
+
+	"github.com/presbrey/goraptor"
 )
 
 type World struct {
 	rasqal_world *C.rasqal_world
-	err os.Error
+	err          error
 }
 
 func NewWorld() (w *World) {
@@ -39,7 +41,7 @@ func GoRasqal_log_handler(user_data unsafe.Pointer, messagep unsafe.Pointer) {
 	world := (*World)(user_data)
 	text := C.GoString(message.text)
 	if C.int(message.level) > C.RAPTOR_LOG_LEVEL_INFO {
-		world.err = os.ErrorString(text)
+		world.err = errors.New(text)
 	}
 	log.Print(text)
 }
@@ -47,7 +49,6 @@ func GoRasqal_log_handler(user_data unsafe.Pointer, messagep unsafe.Pointer) {
 func (w *World) Free() {
 	C.rasqal_free_world(w.rasqal_world)
 }
-
 
 type Query struct {
 	world *World
@@ -65,7 +66,7 @@ func (q *Query) Free() {
 	C.rasqal_free_query(q.query)
 }
 
-func (q *Query) Prepare(query string) (err os.Error) {
+func (q *Query) Prepare(query string) (err error) {
 	cq := C.CString(query)
 	result := C.rasqal_query_prepare(q.query, (*C.uchar)(unsafe.Pointer(cq)), (*C.raptor_uri)(nil))
 	C.free(unsafe.Pointer(cq))
@@ -97,12 +98,12 @@ func QueryPrint(query string) {
 // The rasqal service enables queries against remote SPARQL endpoints.
 // It is a one-off construct, used to execute a single query.
 type Service struct {
-	mutex sync.Mutex
-	world *World
+	mutex    sync.Mutex
+	world    *World
 	endpoint *C.raptor_uri
-	svc *C.rasqal_service
-	dg *C.raptor_sequence
-	www *C.raptor_www
+	svc      *C.rasqal_service
+	dg       *C.raptor_sequence
+	www      *C.raptor_www
 }
 
 func NewService(world *World, endpoint string, query string) *Service {
@@ -114,7 +115,7 @@ func NewService(world *World, endpoint string, query string) *Service {
 	s.endpoint = C.raptor_new_uri(raptor_world, cep)
 	C.free(unsafe.Pointer(cep))
 
-	cquery := C.CString(query)
+	cquery := (*C.uchar)(unsafe.Pointer(C.CString(query)))
 	defer C.free(unsafe.Pointer(cquery))
 
 	s.dg = C.goraptor_new_sequence()
@@ -136,7 +137,7 @@ func NewService(world *World, endpoint string, query string) *Service {
 		return nil
 	}
 
-	s.SetUserAgent("gorasqal hello world")
+	s.SetUserAgent("gorasqal")
 
 	return s
 }
@@ -175,28 +176,28 @@ func (s *Service) SetProxy(proxy string) {
 
 // Perform the operation as a query and return a set of results. This is usually
 // used for SPARUL INSERT/DELETE queries.
-func (s *Service) Execute() (err os.Error) {
+func (s *Service) Execute() (err error) {
 	s.mutex.Lock()
 
 	query_results := C.rasqal_service_execute(s.svc)
 	if query_results == nil {
 		// xxx when this fails, svc gets freed???
 		s.svc = nil
-		err = os.ErrorString("could not execute the query. inspect the log for details")
+		err = errors.New("could not execute the query. inspect the log for details")
 	}
 	s.mutex.Unlock()
 	return
 }
 
 // Perform the operation as a query and return a set of results.
-func (s *Service) Query() (results []map[string]goraptor.Term, err os.Error) {
+func (s *Service) Query() (results []map[string]goraptor.Term, err error) {
 	s.mutex.Lock()
 
 	query_results := C.rasqal_service_execute(s.svc)
 	if query_results == nil {
 		// xxx when this fails, svc gets freed???
 		s.svc = nil
-		err = os.ErrorString("could not execute the query. inspect the log for details")
+		err = errors.New("could not execute the query. inspect the log for details")
 		s.mutex.Unlock()
 		return
 	}
@@ -216,14 +217,14 @@ func (s *Service) Query() (results []map[string]goraptor.Term, err os.Error) {
 		if C.rasqal_query_results_finished(query_results) != 0 {
 			break
 		}
-		
+
 		row := make(map[string]goraptor.Term)
 		for i := 0; i < columns; i++ {
 			rasqal_literal := C.rasqal_query_results_get_binding_value(query_results, C.int(i))
 			ucvalue := C.rasqal_literal_as_string(rasqal_literal)
 			value := C.GoString((*C.char)(unsafe.Pointer(ucvalue)))
 			term_type := C.rasqal_literal_get_rdf_term_type(rasqal_literal)
-			
+
 			var term goraptor.Term
 			switch {
 			case term_type == C.RASQAL_LITERAL_BLANK:
@@ -245,7 +246,7 @@ func (s *Service) Query() (results []map[string]goraptor.Term, err os.Error) {
 				}
 				term = &goraptor.Literal{Value: value, Lang: language, Datatype: datatype}
 			}
-			
+
 			row[bindings[i]] = term
 		}
 
